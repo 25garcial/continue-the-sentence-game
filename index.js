73df13a9a80375ffa02dc0cc3619f13b6bdbfea7
25garcial <91680716+25garcial@users.noreply.github.com>
2022-05-05 14:18:43 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector(".sub").addEventListener("click", function(){main()})
+function main(){
+let fresh=document.querySelector("input").value
+document.querySelector("input").value=""
+	if (fresh.slice(0,1)!=" "){
+  fresh = " " +fresh.slice(0, fresh.length)
+  }
+	
+  document.querySelector("lable").innerHTML=o
+  let separated=fresh.split(" ")
+  for (var i=0; i<separated.length; i++){
+  if (separated[i]=="but") {separated[i]=",but"}}
+  fresh=separated.join("")
+  o+=fresh
+  }
+  
+for (var o="once upon a time,";o.length<80;){
+  main()
+}
+  console.log()
