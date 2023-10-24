211d2fd68926170145affbf2ed4a6dd7c2937b6d
25garcial <91680716+25garcial@users.noreply.github.com>
2023-10-24 13:59:09 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var o = "Once upon a time,";
-document.querySelector(".sub").addEventListener("click", function () { main() })
+var start = "Once upon a time,";
+document.querySelector(".sub").addEventListener("click", function () { main() });
 
 function main() {
-  var freshData = document.querySelector("input").value
-  document.querySelector("input").value = ""
+  var freshData = document.querySelector("input").value;
+  document.querySelector("input").value = "";
   
   if (freshData.slice(0, 1) != " " && freshData.slice(0, 1) != ".") {
-    freshData = " " + freshData.slice(0, freshData.length)
+    freshData = " " + freshData.slice(0, freshData.length);
   }
-  let separated = freshData.split(" ")
+  let separated = freshData.split(" ");
   for (var i = 0; i < separated.length; i++) {
     if (separated[i] == "but") { separated[i - 1] += "," }
     if (separated[i] == "and") { separated[i - 1] += "," }
   }
 
-  freshData = separated.join(" ")
-  o += freshData
+  freshData = separated.join(" ");
+  start += freshData;
 
-  document.querySelector("lable").innerHTML = o
+  document.querySelector("lable").innerHTML = start;
 }
 main()
